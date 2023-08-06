d88c08afa2d39e3cbe2778b3e203810b8681f532
tevy kuch <108342514+t01010k@users.noreply.github.com>
2023-08-06 02:30:06 +0100

    new

--- sim-env/env/game.js
+++ sim-env/env/game.js
@@ -1,28 +1,9 @@
-window.onload = function() {
-    var config = {
-        type: Phaser.AUTO,
-        // edit the size of the bg canvas
-        width: 800,
-        height: 600,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2]
-    };
-
-    //  init () preps the data 
-
-    var game = new Phaser.Game(config);
-
-    function preload() {
-        // Preload assets here, like images, audio, etc into memory
-    }
-
-    function create() {
-        // Create game objects and initialize the scene here.
-        // add game sprites / obs !! wooo!
-        var sprite = this.add.sprite(400, 300, 'your_sprite_key');
-    }
-
-    function update() {
-        // Update the scene. This function is called repeatedly in the game loop + runs constantly
-    }
+var config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2],
+  pixelArt: true
 }
+
+var game = new Phaser.Game(config);
