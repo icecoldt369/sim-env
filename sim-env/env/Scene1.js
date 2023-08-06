fc097408bbdc9ecc324748bd1473cfefcd0953aa
tevy kuch <108342514+t01010k@users.noreply.github.com>
2023-08-06 10:59:56 +0100

    update

--- sim-env/env/Scene1.js
+++ sim-env/env/Scene1.js
@@ -6,10 +6,29 @@ class Scene1 extends Phaser.Scene {
     }
     preload(){
         this.load.image("background", "background.png");
-        this.load.image("ship", "ship.png");
-        this.load.image("ship2", "ship2.png");
-        this.load.image("ship3", "ship3.png");
-        this.load.image("colors", "colors.png");
+        // this.load.image("ship", "ship.png");
+        // this.load.image("ship2", "ship2.png");
+        // this.load.image("ship3", "ship3.png");
+        // this.load.image("colors", "colors.png");
+
+        // spritesheet conversion defined with the size of th pixel frames
+        
+        this.load.spritesheet("ship", "spritesheets/ship.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship2", "spritesheets/ship2.png",{
+            frameWidth: 32,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship3", "spritesheets/ship3.png",{
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet("explosion", "spritesheets/explosion.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     // create text ob
--- sim-env/env/Scene2.js
+++ sim-env/env/Scene2.js
@@ -1,3 +1,5 @@
+
+
 class Scene2 extends Phaser.Scene {
   constructor() {
     super("playGame");
@@ -5,82 +7,102 @@ class Scene2 extends Phaser.Scene {
 
   create() {
 
-    this.iter = 0; // used for itarations
-
     // add the background in the center of the scene
-    this.background = this.add.image(0, 0, "background");
-    this.background.setOrigin(0.5, 0.5);
-    this.background.x = config.width / 2;
-    this.background.y = config.height / 2;
+    // this.background = this.add.image(0, 0, "background");
+    this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
 
-    // Zoow in and out the background
-    this.tweens.add({
-      targets: this.background,
-      duration: 2000,
-      scaleX: 2,
-      scaleY: 2,
-      ease: 'Sine.easeInOut',
-      repeat: -1,
-      yoyo: true
+    // sets the position of the background - good for pivot rotation, corner left size
+    this.background.setOrigin(0,0);
+    
+  //   // Put 3 ships on the canvas - old declaration using image
+  //   this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, "ship");
+  //   this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2").setScale(3);;
+  //   this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3").setScale(3);;
+  //   this.ship2.y -= 100;
+  // }
+
+  // new declaration using sprite
+  this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
+    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");
+  
+     // 0.2 create animations
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3"),
+      frameRate: 20,
+      repeat: -1
     });
 
-    // make a group of ships
-    this.shipGroup = this.make.group({
-      key: "ship",
-      frameQuantity: 56,
-      max: 56
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion"),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
     });
 
-    // align the group of ships in a grid
-    Phaser.Actions.GridAlign(this.shipGroup.getChildren(), {
-      width: 7,
-      height: 9,
-      cellWidth: 32,
-      cellHeight: 32,
-      x: 48,
-      y: 32
+    // 0.3 play the animations
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    // 1 make the ships clickable to destroy them
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    // 1.2
+    this.input.on('gameobjectdown', this.destroyShip, this);
+
+    this.add.text(20, 20, "Playing game", {
+      font: "25px Arial",
+      fill: "yellow"
     });
 
-    // add the color circles
-    this.colors = this.add.image(config.width / 2, config.height / 2, "colors").setScale(3);
-    this.colors.alpha = 1;
-    this.colors.blendMode = 1;
-
-    // Put 3 ships on the
-    this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, "ship");
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2").setScale(3);;
-    this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3").setScale(3);;
-    this.ship2.y -= 100;
   }
 
-
   update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
 
+    this.background.tilePositionY -= 0.5;
+  }
 
-    // rotate the color cicles
-    this.colors.angle += 5;
-
-    // fade in / out the circles
-    this.colors.alpha = Math.sin(this.iter * 3);
-
-    // rotate the ships
-    var children = this.shipGroup.getChildren();
-    for (var i = 0; i < children.length; i++) {
-      children[i].rotation += 0.1;
+// accepts 2 params, one for ship object to move and another for velocity Y
+//  call the resetShipPos from insid when the y position exceeds game height 
+    moveShip(ship, speed){
+        ship.y += speed;
+        if (ship.y > config.height){
+          this.resetShipPos(ship);
+        }
     }
 
-    // animate the 3 big ships
-    this.ship1.rotation = this.iter;
-    this.ship2.x = 90 + Math.sin(this.iter * 10) * 100;
+    // accepts the ship as an obj parameter and sets its Y position to zero
+    // creates a random value between zero and th width of our game and assign it to the ship X value position
+    resetShipPos(ship){
+      ship.y = 0;
+      var randomX = Phaser.Math.Between(0, config.width);
+      ship.x = randomX;
+    }
 
-    this.ship1.scaleX = Math.sin(this.iter * 10) * 5;
-    this.ship1.scaleY = Math.cos(this.iter * 10) * 5;
-
-    this.ship3.rotation = this.iter * 11;
-    this.ship3.y = 140 + Math.sin(this.iter * 10) * 100;
-
-    // increment the iteration
-    this.iter += 0.01;
-
+     // 1.3
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
+  
   }
 }
